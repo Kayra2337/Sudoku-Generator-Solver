1325db4798149734f33e63346e518e8cc1de0577
Kayra2337 <161927412+Kayra2337@users.noreply.github.com>
2025-08-07 18:52:15 +0300

    Update drawer.py

--- drawer.py
+++ drawer.py
@@ -1,28 +1,33 @@
-# sudoku_drawer.py
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def draw_sudoku(board, filename="sudoku.png"):
-    fig, ax = plt.subplots(figsize=(6,6))
-    ax.set_xticks(np.arange(0.5, 9.5, 1))
-    ax.set_yticks(np.arange(0.5, 9.5, 1))
-    ax.grid(which='major', color='black', linewidth=2)
+    fig, ax = plt.subplots(figsize=(6, 6))
 
     # Beyaz arka plan
-    ax.imshow(np.zeros((9,9)), cmap='Blues', vmin=0, vmax=1)
+    ax.set_facecolor('white')
 
-    # Hücrelere siyah renkte rakamları yaz
+    # İnce çizgiler - her hücre arasına
+    for i in range(10):
+        lw = 2 if i % 3 == 0 else 0.8  # 3x3 blok çizgisi kalın, diğerleri ince
+        # Dikey çizgiler
+        ax.plot([i - 0.5, i - 0.5], [-0.5, 8.5], color='black', linewidth=lw)
+        # Yatay çizgiler
+        ax.plot([-0.5, 8.5], [i - 0.5, i - 0.5], color='black', linewidth=lw)
+
+    # Sayıları hücrelerin ortasına yaz
     for i in range(9):
         for j in range(9):
             val = board[i, j]
             if val != 0:
                 ax.text(j, i, str(val), va='center', ha='center', fontsize=20, color='black')
 
-    # Eksenleri gizle
+    # Eksenleri gizle (etiketler, çerçeve vb.)
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_xlim(-0.5, 8.5)
-    ax.set_ylim(8.5, -0.5)  # Y eksenini ters çevir ki üstten başlasın
+    ax.set_ylim(8.5, -0.5)  # Y eksenini ters çevir
 
     plt.savefig(filename)
     plt.close()
